4ba7ed45d3f9475ed8c992adfeec42e4a1ec704d
rutuja <rutujashinde0503@gmail.com>
2026-03-02 12:42:32 +0530

    New Logic in JS

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
 function playGame(userChoice)
 {
-   console.log("user choice is ",userChoice);
+   let userChoice = "rock" ;
+
+   let choices = ["rock","paper","scissors"];
+
+   let randomIndex = Math.floor(Math.random()* 3);
+   let computerChoice = choices[randomIndex];
+
+   console.log("User: " +userChoice);
+   console.log("Computer: "+computerChoice);
+
+   if(userChoice === computerChoice)
+   {
+      console.log("It's a Draw");
+   }
+   else if((userChoice ==="rock" && computerChoice ==="scissors") || 
+          (userChoice ==="paper" && computerChoice ==="rock") ||
+          (userChoice ==="scissors" && computerChoice ==="paper"))
+   {
+      console.log("user Wins!!!!!!");
+   }
+   else
+   {
+      console.log("Computer Wins!!!!!!!!!!");
+   }
+   
 }
